e128bc7bc943a9f1e809ce0e8ad4b415d9bec644
Michael Kuckuk <mdk@kuckuk.com>
2022-03-05 20:14:24 +0100

    Make blackscreen bookmarklet non-intrusive

--- src/bookmarklets/blackscreen.ts
+++ src/bookmarklets/blackscreen.ts
@@ -1,28 +1,59 @@
 export const blackscreen = () => {
+  const animationFrame = () =>
+    new Promise((resolve) => requestAnimationFrame(resolve))
+
+  const openFullscreen = (elem: HTMLElement, onExitFullscreen?: () => void) => {
+    const scheduleExitHandler = (eventName: string) => {
+      if (onExitFullscreen) {
+        document.addEventListener(
+          eventName,
+          () => {
+            if (document.fullscreenElement === null) {
+              onExitFullscreen()
+            } else {
+              scheduleExitHandler(eventName)
+            }
+          },
+          {
+            once: true,
+          },
+        )
+      }
+    }
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen()
+      scheduleExitHandler('fullscreenchange')
+    } else if ((elem as any).webkitRequestFullscreen) {
+      /* Safari */
+      ;(elem as any).webkitRequestFullscreen()
+      scheduleExitHandler('webkitfullscreenchange')
+    } else if ((elem as any).msRequestFullscreen) {
+      /* IE11 */
+      ;(elem as any).msRequestFullscreen()
+      scheduleExitHandler('MSFullscreenChange')
+    }
+  }
+
   const div = document.createElement('div')
   div.style.backgroundColor = 'black'
   div.style.height = '100vh'
   div.style.width = '100vw'
+  div.style.position = 'absolute'
+  div.style.top = '0'
+  div.style.left = '0'
 
-  document.body.style.padding = '0'
-  document.body.style.margin = '0'
-  document.body.style.overflow = 'hidden'
-  document.body.innerHTML = ''
+  const oldOverflowStyle = document.body.style.overflow
 
-  const openFullscreen = (elem: HTMLElement) => {
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen()
-    } else if ((elem as any).webkitRequestFullscreen) {
-      /* Safari */
-      ;(elem as any).webkitRequestFullscreen()
-    } else if ((elem as any).msRequestFullscreen) {
-      /* IE11 */
-      ;(elem as any).msRequestFullscreen()
-    }
+  const onExit = () => {
+    console.log('exit')
+    div.remove()
+    document.body.style.overflow = oldOverflowStyle
   }
 
-  requestAnimationFrame(() => {
+  animationFrame().then(() => {
+    document.body.style.overflow = 'hidden'
+
     document.body.appendChild(div)
-    openFullscreen(document.body)
+    openFullscreen(document.body, onExit)
   })
 }
